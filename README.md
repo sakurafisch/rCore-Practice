dbd954bb7931747efcce0e04629982c49a47cdb4
sakurafisch <sakurafisch@gmail.com>
2021-08-20 23:58:54 +0800

    fix: customize crate for test

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 cargo build --target riscv64gc-unknown-none-elf
 qemu-riscv64 target/riscv64gc-unknown-none-elf/debug/os; echo $?
 ```
+
+## 阅读记录
+
+[自定义测试框架](https://zhuanlan.zhihu.com/p/90710559)
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
 #![no_std]
 #![no_main]
 #![feature(llvm_asm)]
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
 
 mod lang_items;
 
