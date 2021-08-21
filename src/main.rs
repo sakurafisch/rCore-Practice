36831117f2f1fe3ea9ae8f8a51d78b181f643cc8
sakurafisch <sakurafisch@gmail.com>
2021-08-21 12:58:46 +0800

    fix: 清空.bss段

--- src/main.rs
+++ src/main.rs
@@ -78,6 +78,18 @@ macro_rules! println {
     }
 }
 
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| {
+        unsafe {
+            (a as *mut u8).write_volatile(0)
+        }
+    });
+}
+
 // #[no_mangle]
 // extern "C" fn _start() {
 //     println!("Hello World");
