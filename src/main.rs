a5f6cd8cebc36e1e8baa21e8e2c3079dd24449dd
sakurafisch <sakurafisch@gmail.com>
2021-08-21 13:18:46 +0800

    feat: 把裸机打印函数的系统调用改为SBI调用

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,7 @@ use sbi::sbi_call;
 const SYSCALL_EXIT: usize = 93;
 const SYSCALL_WRITE: usize = 64;
 const SBI_SHUTDOWN: usize = 8;
+const SBI_CONSOLE_PUTCHAR: usize = 1;
 
 #[cfg(test)]
 fn test_runner(tests: &[&dyn Fn()]) {
@@ -51,11 +52,18 @@ pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
     syscall(SYSCALL_WRITE, [fd, buffer.as_ptr() as usize, buffer.len()])
 }
 
+pub fn console_putchar(c: usize) {
+    syscall(SBI_CONSOLE_PUTCHAR, [c, 0, 0]);
+}
+
 struct Stdout;
 
 impl Write for Stdout {
     fn write_str(&mut self, s: &str) -> fmt::Result {
-        sys_write(1, s.as_bytes());
+        // sys_write(STDOUT, s.as_bytes());
+        for c in s.chars() {
+            console_putchar(c as usize);
+        }
         Ok(())
     }
 }
@@ -90,6 +98,8 @@ fn clear_bss() {
     });
 }
 
+
+
 // #[no_mangle]
 // extern "C" fn _start() {
 //     println!("Hello World");
@@ -99,5 +109,6 @@ fn clear_bss() {
 
 #[no_mangle]
 pub fn rust_main() -> ! {
+    println!("Hello World");
     shutdown();
 }
